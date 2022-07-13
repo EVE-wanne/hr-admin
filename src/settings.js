f0a2d27e74b80f2e2abb4bc5c568e5d281a25c02
EVE-wanne <1525991994@qq.com>
2022-07-13 11:51:18 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  //* 设置网页标题
+  title: '人力资源后台管理',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
